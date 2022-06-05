e44cb388a890e64fcb4aec54cdb0804c75a401e3
dMedinaO <41024522+dMedinaO@users.noreply.github.com>
2022-06-05 12:39:54 -0800

    Update tape script

--- NLP/Tape/encoding_tape.py
+++ NLP/Tape/encoding_tape.py
@@ -21,7 +21,20 @@ for element in doc_dir:
 
 #processing depending of the response
 if is_exist:
-    data_load = np.load("{}encoding_data.npz".format(path_export))
-    print(data_load)
+    print("Processing dataset")
+    data_load = np.load("{}encoding_data.npz".format(path_export),allow_pickle=True)
+    matrix_data = []
+    for value in list(data_load.keys()):
+        data = data_load[value]
+        values = dict(enumerate(data.flatten(), 1))
+        array_data = [element for element in values[1]['avg']]
+        array_data.insert(0, value)
+        matrix_data.append(array_data)
+
+    print("Exporting data")
+    header = ["p_{}".format(i+1) for i in range(1900)]
+    header.insert(0, "id_seq")
+    df_data = pd.DataFrame(matrix_data, columns=header)
+    df_data.to_csv("{}export_data_encoding.csv".format(path_export), index=False)
 else:
     print("Error with encoder, check the input dataset")
